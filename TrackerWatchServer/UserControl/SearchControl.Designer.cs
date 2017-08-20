597b438e1b192c21055cdd892cff9caa4c8540c5
Emanuela Ventura <venturaemanuela88@gmail.com>
2017-08-20 20:57:56 +0200

    Add SearchControl

---
+++ TrackerWatchServer/UserControl/SearchControl.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TrackerWatchServer
+{
+    partial class SearchControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstDevice = new System.Windows.Forms.ListBox();
+            this.lstUser = new System.Windows.Forms.ListBox();
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lstDevice);
+            this.groupBox1.Controls.Add(this.lstUser);
+            this.groupBox1.Controls.Add(this.txtSearchText);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(345, 459);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ricerca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 249);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dispositivi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 95);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Utenti:";
+            // 
+            // lstDevice
+            // 
+            this.lstDevice.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstDevice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDevice.FormattingEnabled = true;
+            this.lstDevice.ItemHeight = 30;
+            this.lstDevice.Location = new System.Drawing.Point(19, 268);
+            this.lstDevice.Margin = new System.Windows.Forms.Padding(4);
+            this.lstDevice.Name = "lstDevice";
+            this.lstDevice.Size = new System.Drawing.Size(312, 154);
+            this.lstDevice.TabIndex = 3;
+            // 
+            // lstUser
+            // 
+            this.lstUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstUser.FormattingEnabled = true;
+            this.lstUser.ItemHeight = 30;
+            this.lstUser.Location = new System.Drawing.Point(19, 114);
+            this.lstUser.Margin = new System.Windows.Forms.Padding(4);
+            this.lstUser.Name = "lstUser";
+            this.lstUser.Size = new System.Drawing.Size(312, 124);
+            this.lstUser.TabIndex = 2;
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchText.Location = new System.Drawing.Point(19, 46);
+            this.txtSearchText.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(312, 34);
+            this.txtSearchText.TabIndex = 1;
+            this.txtSearchText.TextChanged += new System.EventHandler(this.txtSearchText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserisci un termine per la ricerca:";
+            // 
+            // SearchControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SearchControl";
+            this.Size = new System.Drawing.Size(353, 524);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstDevice;
+        private System.Windows.Forms.ListBox lstUser;
+        private System.Windows.Forms.TextBox txtSearchText;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TrackerWatchServer/UserControl/SearchControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrackerWatchServer
+{
+    public partial class SearchControl : UserControl
+    {
+        List<User> users = new List<User>();
+        List<Device> devices = new List<Device>();
+
+        public SearchControl()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearchText_TextChanged(object sender, EventArgs e)
+        {
+            if (txtSearchText.Text.Length > 0)
+            {
+                users = UserController.SharedInstance.search(txtSearchText.Text);
+                devices = DeviceController.SharedInstance.search(txtSearchText.Text);
+            }
+            updateSearchWithResults(users, devices);
+        }
+
+        private void updateSearchWithResults(List<User> users, List<Device> devices)
+        {
+            //Clean the result list
+            lstUser.Items.Clear();
+            lstDevice.Items.Clear();
+
+            foreach (User user in users)
+            {
+                lstUser.Items.Add(user.Name + " - " + user.Address + " (" + user.City + ")");
+            }
+
+            foreach (Device device in devices)
+            {
+                lstDevice.Items.Add(device.Note + " [" + device.DeviceID + " - " + device.TelephoneNumber + "]");
+            }
+        }
+
+    }
+}
