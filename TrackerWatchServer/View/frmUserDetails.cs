4a6172b7c6228ab9cb97b1047307d8bab34e7a4f
Emanuela Ventura <venturaemanuela88@gmail.com>
2017-08-22 13:36:20 +0200

    frmUserDetails interface

--- TrackerWatchServer/View/frmUserDetails.cs
+++ TrackerWatchServer/View/frmUserDetails.cs
@@ -17,6 +17,114 @@ namespace TrackerWatchServer.View
             InitializeComponent();
         }
 
+        //menu for Contacts
+        private void boldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!rtbContacts.SelectionFont.Bold)
+            {
+                rtbContacts.SelectionFont = new Font(rtbContacts.SelectionFont.OriginalFontName, rtbContacts.SelectionFont.Size, FontStyle.Bold);
+            }
+            else
+            {
+                rtbContacts.SelectionFont = new Font(rtbContacts.SelectionFont.OriginalFontName, rtbContacts.SelectionFont.Size, FontStyle.Regular);
+            }
 
+        }
+
+        private void italicToolStripMenuItemContacts_Click(object sender, EventArgs e)
+        {
+            if (!rtbContacts.SelectionFont.Italic)
+            {
+                rtbContacts.SelectionFont = new Font(rtbContacts.SelectionFont.OriginalFontName, rtbContacts.SelectionFont.Size, FontStyle.Italic);
+            }
+            else
+            {
+                rtbContacts.SelectionFont = new Font(rtbContacts.SelectionFont.OriginalFontName, rtbContacts.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void leftAlignTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtbContacts.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void centerAlignTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtbContacts.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void formatListBullettedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!rtbContacts.SelectionBullet)
+                rtbContacts.SelectionBullet = true;
+            else
+                rtbContacts.SelectionBullet = false;
+        }
+
+        private void characterDimensionToolStripComboBoxContacts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtbContacts.SelectionFont = new System.Drawing.Font(rtbContacts.SelectionFont.FontFamily, float.Parse(characterDimensionToolStripComboBoxContacts.SelectedItem.ToString()), rtbContacts.SelectionFont.Style);
+        }
+
+
+
+
+        //menù for Notes
+        private void boldToolStripMenuItemNotes_Click(object sender, EventArgs e)
+        {
+            if (!rtbNotes.SelectionFont.Bold)
+            {
+                rtbNotes.SelectionFont = new Font(rtbNotes.SelectionFont.FontFamily,rtbNotes.SelectionFont.Size, FontStyle.Bold);
+            }
+            else
+            {
+                rtbNotes.SelectionFont = new Font(rtbNotes.SelectionFont.FontFamily, rtbNotes.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void italicToolStripMenuItemNotes_Click(object sender, EventArgs e)
+        {
+            if (!rtbNotes.SelectionFont.Italic)
+            {
+                rtbNotes.SelectionFont = new Font(rtbNotes.SelectionFont.FontFamily, rtbNotes.SelectionFont.Size, FontStyle.Italic);
+            }
+            else
+            {
+                rtbNotes.SelectionFont = new Font(rtbNotes.SelectionFont.FontFamily, rtbNotes.SelectionFont.Size, FontStyle.Regular);
+            }
+        }
+
+        private void leftAlignTextToolStripMenuItemNotes_Click(object sender, EventArgs e)
+        {
+            rtbNotes.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void centerAlignTextToolStripMenuItemNotes_Click(object sender, EventArgs e)
+        {
+            rtbNotes.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void formatListBullettedToolStripMenuItemNotes_Click(object sender, EventArgs e)
+        {
+            if (!rtbNotes.SelectionBullet)
+                rtbNotes.SelectionBullet = true;
+            else
+                rtbNotes.SelectionBullet = false;
+        }
+
+        private void characterDimensionToolStripComboBoxNotes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            rtbNotes.SelectionFont = new System.Drawing.Font(rtbNotes.SelectionFont.FontFamily, float.Parse(characterDimensionToolStripComboBoxNotes.SelectedItem.ToString()), rtbNotes.SelectionFont.Style);
+        }
+
+
+
+        private void btnAddDevice_Click(object sender, EventArgs e)
+        {
+            frmDevice frm = new frmDevice();
+            frm.Show();
+        }
+
+        
     }
 }
